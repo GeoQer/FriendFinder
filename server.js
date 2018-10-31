b9196deec442261711608346abe2d704ceee5f5c
GeoQer <40548603+GeoQer@users.noreply.github.com>
2018-10-30 20:12:59 -0700

    added middleware

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 let express = require("express");
 let path = require("path");
 
-let app = express;
+let app = express();
+
+let PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
 
